67bfe82dc7c031c631471a2226be3536a2312c27
Brock Ferguson <brock.ferguson@gmail.com>
2012-05-15 17:39:54 -0500

    Updated instructions

--- scantron-process.R
+++ scantron-process.R
@@ -11,7 +11,8 @@
 #   3) Configure the excel_folder and excel_file_csv variables (below) in this R script, to
 #      point to this CSV file.  By default, it will look for excel_file_csv in your current working
 #      directory.
-#   4) Run scantron_process() to create your new analyzeable datafile.  It will output a "processed.csv" in your R
+#	4) Configure the grading_key variable.
+#   5) Run scantron_process() to create your new analyzeable datafile.  It will output a "processed.csv" in your R
 #	   working directory with each student as a row, each question as a column, 1's and 0's marking correct/incorrect,
 #	   and total scores tallied for each question and student.
 #
